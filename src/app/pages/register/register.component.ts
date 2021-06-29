7b5906149674eb7a11f5dd682d5824cf287aa717
victor-singha <vicunite2@gmail.com>
2021-06-29 18:48:25 +0530

    (click) route

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {}
+  btnSignin() {
+    this.router.navigateByUrl('/login');
+  };
+  btnRegister(){
+    this.router.navigateByUrl('/login');
+  }
 
   ngOnInit(): void {
+    
   }
 
 }
