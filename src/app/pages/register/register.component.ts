054938127bce16cc2ab79625b91366e95e2d51ad
victor-singha <vicunite2@gmail.com>
2021-06-30 12:37:07 +0530

    password eye

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
@@ -18,5 +19,12 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
     
   }
+  signin: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.email, Validators.required ]),
+    password: new FormControl('', [Validators.required, Validators.min(3) ])
+  });
+  hide = true;
+  get emailInput() { return this.signin.get('email'); }
+  get passwordInput() { return this.signin.get('password'); }
 
 }
