921591b8d95d762842961d4e3e939ed999b09086
victor-singha <vicunite2@gmail.com>
2021-07-04 13:44:26 +0530

    [victor] register pass cpass refactor

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -8,7 +8,10 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
     const invalidCtrl = !!(control && control.invalid && control.parent?.dirty);
     const invalidParent = !!(control && control.parent && control.parent.invalid && control.parent.dirty);
-
+    console.log("control invalid",control?.invalid)
+    console.log("parent dirty",control?.parent?.dirty)
+    console.log(form?.control.get('confirmpassword')?.value)
+    if(form?.control.get('confirmpassword')?.value == form?.control.get('password')?.value) return false;
     return (invalidCtrl || invalidParent);
   }
 }
@@ -18,26 +21,26 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  
   matcher = new MyErrorStateMatcher();
-
   constructor(private router: Router,private userservice: UserService,private formBuilder: FormBuilder) {}
 
   btnSignin() {
     this.router.navigateByUrl('/login');
   };
+  
   btnRegister(){
     // this.router.navigateByUrl('/login');
     if(this.registerForm.valid){
       console.log(this.registerForm.value)
       // TODO 
-      this.userservice.registration(this.registerForm.value).subscribe(data=>{console.log(data)})
+      // this.userservice.registration(this.registerForm.value).subscribe(data=>{console.log(data)})
     
     }
   }
   
-  ngOnInit(): void {
+  ngOnInit(): void {}
 
-  }
   registerForm = this.formBuilder.group({
     firstname: new FormControl(null, [Validators.required]),
     lastname: new FormControl(null, [Validators.required]),
@@ -45,12 +48,12 @@ export class RegisterComponent implements OnInit {
     password: new FormControl(null, [Validators.required, Validators.min(3)]),
     confirmpassword: new FormControl(null, [Validators.required, Validators.min(3)]),
   },{ validators: this.checkPasswords });
+  
   hide = true;
 
   checkPasswords(control: AbstractControl): ValidationErrors | null { // here we have the 'passwords' group
     const pass = control.get('password')?.value;
     const confirmPass = control.get('confirmpassword')?.value;
-    console.log(`pass=${pass} conpass=${confirmPass}`)
     return pass === confirmPass ? null : { notSame: true }     
   }
 
