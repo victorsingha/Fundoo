df98e48eb35c0c5d5a010758682bd8b6b422df63
victor-singha <vicunite2@gmail.com>
2021-07-08 14:22:00 +0530

    [victor] get Notes from API

--- src/app/components/notes/notes.component.ts
+++ src/app/components/notes/notes.component.ts
@@ -15,35 +15,36 @@ export class NotesComponent implements OnInit {
   receiveMessage($event: any) {
     this.note = $event
     console.log(this.note)
-    this.notes.push(this.note)
+    // this.notes.push(this.note)
   }
-  // notes:any;
-
+  notes:any;
+  token: any;
   ngOnInit(): void {
-    //  this.getAllNotes("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFbWFpbCI6InN1cC5waWNraXR1cEBnbWFpbC5jb20iLCJVc2VySUQiOiIxMCIsIm5iZiI6MTYyNTcxNjg0NCwiZXhwIjoxNjI1NzIwNDQ0LCJpYXQiOjE2MjU3MTY4NDR9.9GVVcrT0v6agm4MU_OrASKBSt3Gq1k7hsrd3bItGIqM")
-    //  .subscribe(res=>{
-    //    console.log(res);
-    //    this.notes = res;
-    //   })
+    this.token = localStorage.getItem("token");
+    this.getAllNotes(this.token)
+    .subscribe(res=>{
+      console.log(res);
+      this.notes = res;
+    })
   }
-  notes = [
-    { "title": "hello111 111111111111111 222222222 3333333333333", "body": "Available1" },
-    { "title": "hello2", "body": "Available2" },
-    { "title": "hello3", "body": "Take the bag of rice and send to Ben. Take the bag of rice and send to Ben." },
-    { "title": "hello4", "body": "Available4" },
-    { "title": "hello5", "body": "Available5" },
-    { "title": "hello6", "body": "Take the bag of rice and send to Ben. Take the bag of rice and send to Ben.Take the bag of rice and send to Ben. Take the bag of rice and send to Ben." },
-    { "title": "hello7", "body": "Available7" },
-    { "title": "hello8", "body": "Available8" },
-];
+//   notes = [
+//     { "title": "hello111 111111111111111 222222222 3333333333333", "body": "Available1" },
+//     { "title": "hello2", "body": "Available2" },
+//     { "title": "hello3", "body": "Take the bag of rice and send to Ben. Take the bag of rice and send to Ben." },
+//     { "title": "hello4", "body": "Available4" },
+//     { "title": "hello5", "body": "Available5" },
+//     { "title": "hello6", "body": "Take the bag of rice and send to Ben. Take the bag of rice and send to Ben.Take the bag of rice and send to Ben. Take the bag of rice and send to Ben." },
+//     { "title": "hello7", "body": "Available7" },
+//     { "title": "hello8", "body": "Available8" },
+// ];
 
 
-// getAllNotes(auth_token: string){
-//   const headers= new HttpHeaders()
-//   .append('content-type', 'application/json')
-//   .append('Access-Control-Allow-Origin', '*')
-//   .append('Authorization',`Bearer ${auth_token}`);
-//   return this.http
-//       .get("https://localhost:44354/api/notes/list",{ 'headers': headers })  
-// }
+getAllNotes(auth_token:string | null){
+  const headers= new HttpHeaders()
+  .append('content-type', 'application/json')
+  .append('Access-Control-Allow-Origin', '*')
+  .append('Authorization',`Bearer ${auth_token}`);
+  return this.http
+      .get("https://localhost:44354/api/notes/list",{ 'headers': headers })  
+}
 }
