a7be185e9f8df7e6424252c47c8b51dc79f400ba
victor-singha <vicunite2@gmail.com>
2021-07-08 09:54:20 +0530

    [victor] note component wordwrap and icon fix

--- src/app/components/notes/notes.component.ts
+++ src/app/components/notes/notes.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,12 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class NotesComponent implements OnInit {
 
   constructor(private http:HttpClient) { }
-
+  // notes:any;
   ngOnInit(): void {
-    // this.http.get("")
+    //  this.getAllNotes("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFbWFpbCI6InN1cC5waWNraXR1cEBnbWFpbC5jb20iLCJVc2VySUQiOiIxMCIsIm5iZiI6MTYyNTcxNjg0NCwiZXhwIjoxNjI1NzIwNDQ0LCJpYXQiOjE2MjU3MTY4NDR9.9GVVcrT0v6agm4MU_OrASKBSt3Gq1k7hsrd3bItGIqM")
+    //  .subscribe(res=>{
+    //    console.log(res);
+    //    this.notes = res;
+    //   })
   }
   notes: { title: string, body: string }[] = [
-    { "title": "hello1", "body": "Available1" },
+    { "title": "hello111 111111111111111 222222222 3333333333333", "body": "Available1" },
     { "title": "hello2", "body": "Available2" },
     { "title": "hello3", "body": "Take the bag of rice and send to Ben. Take the bag of rice and send to Ben." },
     { "title": "hello4", "body": "Available4" },
@@ -22,6 +26,15 @@ export class NotesComponent implements OnInit {
     { "title": "hello6", "body": "Take the bag of rice and send to Ben. Take the bag of rice and send to Ben.Take the bag of rice and send to Ben. Take the bag of rice and send to Ben." },
     { "title": "hello7", "body": "Available7" },
     { "title": "hello8", "body": "Available8" },
-
 ];
+
+
+// getAllNotes(auth_token: string){
+//   const headers= new HttpHeaders()
+//   .append('content-type', 'application/json')
+//   .append('Access-Control-Allow-Origin', '*')
+//   .append('Authorization',`Bearer ${auth_token}`);
+//   return this.http
+//       .get("https://localhost:44354/api/notes/list",{ 'headers': headers })  
+// }
 }
