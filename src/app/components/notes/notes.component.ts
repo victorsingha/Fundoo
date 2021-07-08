2bf279527ea6f5b6d7504c613a648f7e8b01deab
victor-singha <vicunite2@gmail.com>
2021-07-08 08:35:49 +0530

    [victor] content height and overflow

--- src/app/components/notes/notes.component.ts
+++ src/app/components/notes/notes.component.ts
@@ -14,10 +14,10 @@ export class NotesComponent implements OnInit {
   notes: { title: string, body: string }[] = [
     { "title": "hello1", "body": "Available1" },
     { "title": "hello2", "body": "Available2" },
-    { "title": "hello3", "body": "Available3" },
+    { "title": "hello3", "body": "Take the bag of rice and send to Ben. Take the bag of rice and send to Ben." },
     { "title": "hello4", "body": "Available4" },
     { "title": "hello5", "body": "Available5" },
-    { "title": "hello6", "body": "Available6" },
+    { "title": "hello6", "body": "Take the bag of rice and send to Ben. Take the bag of rice and send to Ben.Take the bag of rice and send to Ben. Take the bag of rice and send to Ben." },
     { "title": "hello7", "body": "Available7" },
     { "title": "hello8", "body": "Available8" },
 
