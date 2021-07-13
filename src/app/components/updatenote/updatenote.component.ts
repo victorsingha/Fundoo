9aeecbb180231b70aab917c2989ae1aed4d67283
victor-singha <vicunite2@gmail.com>
2021-07-13 20:48:15 +0530

    [victor] data populate in mat-dialog

--- src/app/components/updatenote/updatenote.component.ts
+++ src/app/components/updatenote/updatenote.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
 @Component({
@@ -10,9 +11,19 @@ export class UpdatenoteComponent implements OnInit {
 
   constructor( public dialogRef: MatDialogRef<UpdatenoteComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any) { }
-
+    updateForm: FormGroup = new FormGroup({});
   ngOnInit(): void {
     console.log(this.data)
+    this.updateForm = new FormGroup({
+      Title: new FormControl(this.data.title),
+      Body: new FormControl(this.data.body),
+      Reminder: new FormControl("default"),
+      Color: new FormControl("white"),
+      isArchived: new FormControl(false),
+      isTrash: new FormControl(false),
+      isPin: new FormControl(false),
+      UserId: new FormControl(0)
+    });
   }
   close(){
     console.log("close-dialog")
